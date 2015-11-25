b6f465793654599991fea9153fc23b0f0f0f8449
chickensmitten <chia.shengyeong@gmail.com>
2015-11-25 09:50:22 +0800

    finished tutorial

--- app/api/employee/data.rb
+++ app/api/employee/data.rb
@@ -6,8 +6,46 @@ module Employee
       desc "List all Employee"
 
       get do 
-        EmpData.all
+        EmpDatum.all
       end
+
+      desc "create a new employee"
+      ## This takes care of parameter validation
+      params do
+        requires :name, type: String
+        requires :address, type:String
+        requires :age, type:Integer
+      end
+      ## This takes care of creating employee
+      post do
+        EmpDatum.create!({
+          name:params[:name],
+          address:params[:address],
+          age:params[:age]
+        })
+      end
+      
+      desc "delete an employee"
+      
+      params do
+        requires :id, type: String
+      end
+      
+      delete ':id' do
+        EmpDatum.find(params[:id]).destroy!
+      end
+
+      desc "update an employee address"
+      params do
+        requires :id, type: String
+        requires :address, type:String
+      end
+      put ':id' do
+        EmpDatum.find(params[:id]).update({
+          address:params[:address]
+        })
+      end
+
 
     end
 
