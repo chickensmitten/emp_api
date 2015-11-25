c641d643e9764bb68f0e16034b5434ef334fd08f
chickensmitten <chia.shengyeong@gmail.com>
2015-11-25 09:14:14 +0800

    first commit

---
+++ app/api/employee/data.rb
@@ -0,0 +1,32 @@
+module Employee
+  class Data < Grape::API
+    
+    resource :employee_data do
+      
+      desc "List all Employee"
+
+      get do 
+        EmpData.all
+      end
+
+    end
+
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
